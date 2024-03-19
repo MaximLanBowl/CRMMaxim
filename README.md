d0dbe13e708a06621fe96e2230704f986a6faf92
Максим Ландау <12landay@mail.ru>
2024-03-19 14:34:39 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CRMMaxim
+# MainMenu
--- myapp/uslugi/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Product(models.Model):
-    objects = None
-    name = models.CharField(null=False, primary_key=True, max_length=80)
-    description = models.CharField(null=False)
-    price = models.IntegerField()
